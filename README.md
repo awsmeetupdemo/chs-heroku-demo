e236d4d3b6767b86c7604ad1617a5be4971d6141
Thomas Ruggeri <truggeri@gmail.com>
2020-01-18 19:48:23 -0500

    Minor updates
    Favicon
    Using Picsum for random image
    Adding some text changes
    Removing color from name

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CHS Heroku Demo
 
 A _very_ simple Heroku demo for the [Charleston Aws Meetup](https://www.meetup.com/CHS-Amazon-Web-Services/).
+View it on Heroku at [https://chs-heroku-demo.herokuapp.com](https://chs-heroku-demo.herokuapp.com)
--- app/helpers/guest_book_helper.rb
+++ app/helpers/guest_book_helper.rb
@@ -1,9 +1,19 @@
 module GuestBookHelper
-  LOREM_PATH   = 'https://loremflickr.com'.freeze
+  FLICKR_PATH  = 'https://loremflickr.com'.freeze
   SEARCH_TERMS = 'animal,dog,cat'.freeze
+  PICSUM_PATH  = 'https://picsum.photos'.freeze
 
   def avitar_image(seed: 0, width: 100, height: 160)
-    location = safe_join([LOREM_PATH, width, height, SEARCH_TERMS], '/') + "?lock=#{seed}"
+    picsum_image(seed: seed, width: width, height: height)
+  end
+
+  def picsum_image(seed: 0, width: 100, height: 160)
+    location = safe_join([PICSUM_PATH, 'seed', seed, width, height], '/')
+    image_tag(location, width: width, height: height, alt: 'Avitar')
+  end
+
+  def flicker_image(seed: 0, width: 100, height: 160)
+    location = safe_join([FLICKR_PATH, width, height, SEARCH_TERMS], '/') + "?lock=#{seed}"
     image_tag(location, width: width, height: height, alt: 'Avitar')
   end
 end
