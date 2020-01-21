2f5b9e8f385b2aee935ae12763237da5584ef105
Thomas Ruggeri <truggeri@gmail.com>
2020-01-21 14:58:04 -0500

    Adding configurable color theme

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module ChsHerokuDemo
+  COLOR_THEME = 'purple'.freeze
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
